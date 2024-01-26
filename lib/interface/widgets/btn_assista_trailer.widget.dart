6f1c3191a33e7d1ee864b8022e6d1b7d3363caf1
isabelafagundes <isabelahidalgo.2004@gmail.com>
2024-01-26 11:43:05 -0300

    Adicionando callback nos botões

--- lib/interface/widgets/btn_assista_trailer.widget.dart
+++ lib/interface/widgets/btn_assista_trailer.widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class BtnAssistaTrailer extends StatelessWidget {
   const BtnAssistaTrailer({Key? key}) : super(key: key);
@@ -7,29 +8,37 @@ class BtnAssistaTrailer extends StatelessWidget {
   Widget build(BuildContext context) {
     return MouseRegion(
       cursor: SystemMouseCursors.click,
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          Image.asset("assets/img/btn_losango_vazado.png"),
-          Container(
-            width: 170,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  "Assista o trailer".toUpperCase(),
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.white,
-                    fontFamily: "Poppins",
-                    fontWeight: FontWeight.w500,
+      child: GestureDetector(
+        onTap: () async {
+          await launchUrl(
+            Uri.parse("https://youtu.be/ByXuk9QqQkk?si=EQ2tzAMFLJcVhuYM"),
+            mode: LaunchMode.externalApplication,
+          );
+        },
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Image.asset("assets/img/btn_losango_vazado.png"),
+            Container(
+              width: 170,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "Assista o trailer".toUpperCase(),
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                      fontFamily: "Poppins",
+                      fontWeight: FontWeight.w500,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/interface/widgets/btn_assistir_agora.widget.dart
+++ lib/interface/widgets/btn_assistir_agora.widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class BtnAssistirAgora extends StatelessWidget {
   const BtnAssistirAgora({Key? key}) : super(key: key);
@@ -7,34 +8,42 @@ class BtnAssistirAgora extends StatelessWidget {
   Widget build(BuildContext context) {
     return MouseRegion(
       cursor: SystemMouseCursors.click,
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          Image.asset("assets/img/btn_losango.png"),
-          Container(
-            width: 170,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.play_arrow,
-                  color: Colors.white,
-                ),
-                SizedBox(width: 10),
-                Text(
-                  "Assistir agora".toUpperCase(),
-                  style: TextStyle(
-                    fontSize: 16,
+      child: GestureDetector(
+        onTap: () async {
+          await launchUrl(
+            Uri.parse("https://web.prod.ftl.netflix.com/br/title/60023642"),
+            mode: LaunchMode.externalApplication,
+          );
+        },
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Image.asset("assets/img/btn_losango.png"),
+            Container(
+              width: 170,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.play_arrow,
                     color: Colors.white,
-                    fontFamily: "Poppins",
-                    fontWeight: FontWeight.w500,
                   ),
-                ),
-              ],
+                  SizedBox(width: 10),
+                  Text(
+                    "Assistir agora".toUpperCase(),
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                      fontFamily: "Poppins",
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
